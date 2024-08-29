fbb88c5a40c91ade0bf3689c0512c8de729d4035
EngSamirah <124391204+EngSamirah@users.noreply.github.com>
2024-08-29 11:12:03 +0300

    Delete ollama-tutorial directory

--- ollama-tutorial/main.py
+++
@@ -1,7 +0,0 @@
-import ollama
-
-response = ollama.generate(model='gemma:2b',
-                          prompt='what is a qubit?')
-print(response['response'])
-
-
--- ollama-tutorial/main1.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.llms import Ollama
-
-llm = Ollama(model="llama2")
-
-llm.invoke("tell me about partial functions in python")
